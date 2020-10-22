712e2d4f925405779e653bded247f724237e9c84
AGEfx <33631984+AGEfx@users.noreply.github.com>
2020-10-22 23:50:12 +0300

    Update 3)bubbleSort.r
    cosmetic improvements

--- 3) Bubble sort/3)bubbleSort.r
+++ 3) Bubble sort/3)bubbleSort.r
@@ -1,15 +1,15 @@
-bubbleSort <- function(arr){
-  len = length(arr) 
+bubble_sort <- function(vector){
+  len <- length(vector) 
   for (i in 1:(len - 1)) {
     for (j in 1:(len - 1)) {
-      if (arr[j] > arr[j + 1]){
-        temp = arr[j]
-        arr[j] = arr[j + 1]
-        arr[j + 1] = temp
+      if (vector[j] > vector[j + 1]){
+        temp <- vector[j]
+        vector[j] <- vector[j + 1]
+        vector[j + 1] <- temp
       }
     }
   }
-  return(arr)
+  return(vector)
 }
-print(bubbleSort(c(3,-4,3,100,-100,-8,55,9,10,134))) # output: -100   -8   -4    3    3    9   10   55  100  134
+print(bubble_sort(c(3,-4,3,100,-100,-8,55,9,10,134))) # output: -100   -8   -4    3    3    9   10   55  100  134
 
