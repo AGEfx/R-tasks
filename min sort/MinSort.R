737bbaac3ff97dd8cccc16c90527b3609243de1e
AGEfx <33631984+AGEfx@users.noreply.github.com>
2020-12-18 21:56:12 +0300

    Update MinSort.R

--- min sort/MinSort.R
+++ min sort/MinSort.R
@@ -1,23 +1,23 @@
 vector <- c(-1,3,6,2,0,-13,-3,3,64)
-new_vector <- c()
-
-min <- 0
-min_pos <- 0
-
-for (i in 1:length(vector)){
-  min <- vector[1]
-  min_pos <- 1
-  for (j in 1:length(vector)){
-    if(vector[j] < min){ 
-      min <- vector[j] 
-      min_pos <- j
-    }
-  }  
-  new_vector[i] <- min 
-  vector <- vector[-min_pos] #удаление элемента с индексом min_pos из вектора
-  
+sort <- function(vector){
+  new_vector <- c()
+  for (i in 1:length(vector)){
+    min <- vector[1]
+    min_pos <- 1
+    for (j in 1:length(vector)){
+      if(vector[j] < min){ 
+        min <- vector[j] 
+        min_pos <- j
+      }
+    }  
+    new_vector[i] <- min 
+    vector <- vector[-min_pos] #удаление элемента с индексом min_pos из вектора
+    
+  }
+  return(new_vector)
 }
 
-new_vector
+print(sort(vector))
+
 
 #output: -13  -3  -1   0   2   3   3   6  64
