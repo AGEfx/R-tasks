20fbd719fe3eddfd92c32829bf6cc7d760211bc2
AGEfx <33631984+AGEfx@users.noreply.github.com>
2022-10-13 12:57:25 +0300

    Update HomeTask.java

--- HomeTask.java
+++ HomeTask.java
@@ -1,19 +1,24 @@
-package Test;
-
-public class HomeTask {
+public class Main {
     //y - лямбда - среднее число заказов в день
     int y = 3;
     public static void main(String[] args) {
-        HomeTask homeTask = new HomeTask();
+        Main main = new Main();
 
-        //1) вероятность размещения заказа к концу каждого дня недели
+        //1) вероятность размещения заказа к концу каждого дня недели(t)
         for (int t = 1; t <= 7; t++) {
             //вероятность, что за день будет совершено 0 заказов
-            double p0 = 1.0 - homeTask.nSum(18,t);
+            double p0 = 1.0 - main.nSum(18,t);
             //вероятность, что за день будет совершено <=5 заказов
-            double res = p0 + homeTask.nSum(5,t);
-            System.out.println("День " + t + ": " + res);
+            double result_p = p0 + main.nSum(5,t);
+            System.out.println("День " + t + ": " + result_p);
         }
+
+        //2)Среднее количество единиц товара, которые будут списаны к концу недели
+        double avg_number = 0.0;
+        for (int n = 0; n < 18; n++) {
+            avg_number += n * main.p(18-n,7);
+        }
+        System.out.println("Среднее количество единиц товара = " + avg_number);
     }
 
     //функция подсчета вероятности
@@ -29,6 +34,8 @@ public class HomeTask {
         else
             return (k * factorial(k - 1));
     }
+
+
 
     //функция суммы
     double nSum(int n, int t){
